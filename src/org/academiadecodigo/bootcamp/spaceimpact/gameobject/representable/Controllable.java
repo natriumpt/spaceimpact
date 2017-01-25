4a24e50c261b12b54d26eb20eb9d2ffd3503eb07
Rute <rute_carrapato@outlook.pt>
2017-01-25 14:18:19 +0000

    Update on the keyboard

---
+++ src/org/academiadecodigo/bootcamp/spaceimpact/gameobject/representable/Controllable.java
@@ -0,0 +1,7 @@
+package org.academiadecodigo.bootcamp.spaceimpact.gameobject.representable;
+
+/**
+ * Created by codecadet on 1/25/17.
+ */
+public interface Controlable {
+}
---
+++ src/org/academiadecodigo/bootcamp/spaceimpact/simplegfx/SimpleGfxKeyboard.java
@@ -0,0 +1,7 @@
+package org.academiadecodigo.bootcamp.spaceimpact.simplegfx;
+
+/**
+ * Created by codecadet on 1/25/17.
+ */
+public class SimpleGfxKeyboard {
+}
