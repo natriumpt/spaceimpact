221447162852dc790b29006c35f2b6d63cd6ec59
filipemcbranco <filipemcbranco@gmail.com>
2017-01-25 18:08:34 +0000

    dynamic background - SimpleGfxField

--- src/org/academiadecodigo/bootcamp/spaceimpact/simplegfx/SimpleGfxField.java
+++ src/org/academiadecodigo/bootcamp/spaceimpact/simplegfx/SimpleGfxField.java
@@ -4,13 +4,36 @@ import org.academiadecodigo.simplegraphics.pictures.Picture;
 
 public class SimpleGfxField extends SimpleGfxRepresentable {
 
-    //TODO implement pictureArray to simulate animation of background
-
+    private int pictureStateMachine;
     private Picture[] pictureArray;
 
     public SimpleGfxField(int x, int y) {
-        super(new Picture(x, y, "background01.png"));
+        super(new Picture(x,y,"background1.png"));
         this.pictureArray = new Picture[30];
+        pictureStateMachine = 1;
+        fillPictureArray(x,y);
     }
+
+    public void fillPictureArray(int x, int y){
+        for (int i = 1; i < pictureArray.length ; i++){
+                pictureArray[i] = new Picture(x,y,"background" + i +".png");
+        }
+    }
+
+    public void stateMachinePictureChanging() {
+        Picture picture;
+
+        if (pictureStateMachine == 30)
+            pictureStateMachine = 1;
+        picture = pictureArray[pictureStateMachine];
+        picture.draw();
+        pictureStateMachine++;
+    }
+
+
+
+
+
+
 
 }
