bc31d1a293d7c269d4d9610cc5e8b786e4db4fd8
filipemcbranco <filipemcbranco@gmail.com>
2017-01-26 14:47:05 +0000

    Class HighScore added

---
+++ src/org/academiadecodigo/bootcamp/spaceimpact/gameobject/HighScore.java
@@ -0,0 +1,24 @@
+package org.academiadecodigo.bootcamp.spaceimpact.gameobject;
+
+import java.security.PublicKey;
+
+/**
+ * Created by filipe on 26-01-2017.
+ */
+public class HighScore {
+
+    //TODO: Assigned to Branco
+
+    Player player;
+
+    public HighScore(Player player){
+        this.player = player;
+    }
+
+
+    public void setHighScore(){
+
+    }
+
+
+}
