3bcb3f00178025d8068402b6940a2bdd586187c6
filipemcbranco <filipemcbranco@gmail.com>
2017-01-27 14:23:04 +0000

    highScore changes

--- src/org/academiadecodigo/bootcamp/spaceimpact/gameobject/HighScore.java
+++ src/org/academiadecodigo/bootcamp/spaceimpact/gameobject/HighScore.java
@@ -1,6 +1,9 @@
 package org.academiadecodigo.bootcamp.spaceimpact.gameobject;
 
+import java.io.*;
 import java.security.PublicKey;
+
+import static java.lang.System.in;
 
 /**
  * Created by filipe on 26-01-2017.
@@ -13,6 +16,23 @@ public class HighScore {
 
     public HighScore(Player player){
         this.player = player;
+    }
+
+
+    public void isHighscore(int score) throws IOException{
+         FileInputStream inputStream = new FileInputStream("highscores.txt");;
+
+         byte[] buffer = new byte[1024]; //nova linha
+         int line = inputStream.read(buffer);
+
+         // hashmap key value
+
+         while (line != '\0'){
+
+         }
+
+
+         inputStream.close();
     }
 
 
