af91d0f17630065b77d7b3294a645916a3259f25
Alexandre Ramos <natrium.pt@gmail.com>
2017-01-27 10:26:35 +0000

    Enemy pattern changes

--- src/org/academiadecodigo/bootcamp/spaceimpact/gameobject/Enemy.java
+++ src/org/academiadecodigo/bootcamp/spaceimpact/gameobject/Enemy.java
@@ -4,23 +4,25 @@ import org.academiadecodigo.bootcamp.spaceimpact.gameobject.representable.Repres
 
 public class Enemy extends Ship {
 
-    ProjectileFactory projectileFactory;
-    private int currentPattern;
+    private ProjectileFactory projectileFactory;
     private TargetPosition targetPosition;
-    private int [] stepsSmoothing = new int [2];
+    private int currentPattern;
+    private int[] stepsSmoothing = new int[2];
 
     public Enemy(Representable representation, int x, int y, int w, int h, ProjectileFactory projectileFactory) {
         super(representation, x, y, w, h);
         this.projectileFactory = projectileFactory;
         setHitPoints(3);
-        setSpeed(1); // TODO: Change this to reasonable values
-        targetPosition = new TargetPosition(100,100);
+        setSpeed(20); // TODO: Change this to reasonable values
+        targetPosition = new TargetPosition(100, 100);
     }
 
     public void enemyMove(int x, int y) {
         // TODO: Change this to ensure more realistic diagonal movement
         if (getX() != x || getY() != y) {
+            System.out.println("Position is different");
             if (getX() > x) {
+                System.out.println("Position greater than target");
                 move(-getSpeed(), 0);
             }
             if (getX() < x) {
@@ -37,37 +39,58 @@ public class Enemy extends Ship {
 
     @Override
     public void fire() {
-        projectileFactory.createProjectile(GameObjectType.PROJECTILE, getX(), getY(), 16, 4, false, 1, 100);
+        projectileFactory.createProjectile(GameObjectType.PROJECTILE, getX(), getY(), 16, 4, false, 1, 16);
+    }
+
+    public void newPattern() {
+        switch (currentPattern) {
+            case 0:
+                if (getX() != 50 && getY() != 50) {
+                    enemyMove(50, 50);
+                    System.out.println("Attempting move 50 50");
+                } else {
+                    fire();
+                    System.out.println("Attempting fire");
+                    currentPattern = 1;
+                }
+            case 1:
+                if (getX() != 1000 && getY() != 200) {
+                    enemyMove(1000, 200);
+                } else {
+                    fire();
+                    currentPattern = 0;
+                }
+        }
     }
 
     public void pattern() {
         // TODO: Implement a pattern for the enemy
 
-        if (!inTargetPosition()){
-            move(getSpeed()*stepsSmoothing[0],getSpeed()*stepsSmoothing[1]);
+        if (!inTargetPosition()) {
+            move(getSpeed() * stepsSmoothing[0], getSpeed() * stepsSmoothing[1]);
         }
 
-        switch (currentPattern){
+        switch (currentPattern) {
 
             case 0:
                 fire();
                 targetPosition.setTargetX(50);
                 targetPosition.setTargetY(50);
-                stepsCalc(targetPosition.getTargetX(),targetPosition.getTargetY());
-                currentPattern++;
+                stepsCalc(targetPosition.getTargetX(), targetPosition.getTargetY());
+                // currentPattern++;
                 break;
             case 1:
                 fire();
                 targetPosition.setTargetX(100);
                 targetPosition.setTargetY(100);
-                stepsCalc(targetPosition.getTargetX(),targetPosition.getTargetY());
+                stepsCalc(targetPosition.getTargetX(), targetPosition.getTargetY());
                 currentPattern++;
                 break;
             case 2:
                 fire();
                 targetPosition.setTargetX(150);
                 targetPosition.setTargetY(150);
-                stepsCalc(targetPosition.getTargetX(),targetPosition.getTargetY());
+                stepsCalc(targetPosition.getTargetX(), targetPosition.getTargetY());
                 currentPattern++;
                 break;
             default:
@@ -77,7 +100,7 @@ public class Enemy extends Ship {
         }
     }
 
-    public int [] stepsCalc(int targetX , int targetY){
+    public int[] stepsCalc(int targetX, int targetY) {
         int enemyCurX = getX();
         int enemyCurY = getY();
 
@@ -87,7 +110,7 @@ public class Enemy extends Ship {
         int xIncrement;
         int yIncrement;
 
-        int movePattern [] =  new int[2];
+        int movePattern[] = new int[2];
 
         int dx = goalX - enemyCurX;
         int dy = goalY - enemyCurY;
@@ -97,12 +120,12 @@ public class Enemy extends Ship {
         if (Math.abs(dx) > Math.abs(dy)) {
             steps = Math.abs(dx);
         } else {
-            steps =Math.abs(dy);
+            steps = Math.abs(dy);
         }
 
 
-        xIncrement = dx/steps;
-        yIncrement = dy/steps;
+        xIncrement = dx / steps;
+        yIncrement = dy / steps;
 
         movePattern[0] = xIncrement;
         movePattern[1] = yIncrement;
@@ -111,9 +134,12 @@ public class Enemy extends Ship {
 
     }
 
-    public boolean inTargetPosition(){
-        return (this.getX()==this.targetPosition.getTargetX() && this.getY() == this.targetPosition.getTargetY());
+    public boolean inTargetPosition() {
+        return (this.getX() == this.targetPosition.getTargetX() && this.getY() == this.targetPosition.getTargetY());
     }
 
-
+    @Override
+    public String toString() {
+        return "Enemy " + getX() + " " + getY() + " HP:" + getHitPoints();
+    }
 }
